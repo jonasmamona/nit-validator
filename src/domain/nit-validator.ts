f7fe4413225348b5aa43ebbe6725bdc314604b19
jonas <jonasmpdc@gmail.com>
2023-05-16 19:44:17 -0300

    now properly calculating dv!

--- src/domain/nit-validator.ts
+++ src/domain/nit-validator.ts
@@ -1,6 +1,10 @@
 import { Either, makeLeft, makeRight } from "../helper/either-monad";
 
-type ValidationError = "TooShort" | "TooLong" | "InvalidFormat" | "InvalidRange";
+type ValidationError =
+  | "TooShort"
+  | "TooLong"
+  | "InvalidFormat"
+  | "InvalidRange";
 
 type ValidationResult = {
   nit: string;
@@ -22,15 +26,22 @@ function assembleErrorString(errors: ValidationError[]): string {
   return errors.map(convertValidationErrorToHelpText).join(", ");
 }
 
+function addZeroesToLeft(nit: string): string {
+  if (nit.length < 15) {
+    return addZeroesToLeft("0" + nit);
+  } else {
+    return nit;
+  }
+}
+
 function calculateNITVerificationDigit(
   nit: string
 ): Either<ValidationError[], ValidationResult> {
   const nitRegex = /^[0-9]{1,13}-?[0-9]$/;
-  const nitDigits = nit.replace(/-/g, "");
 
   let errors: ValidationError[] = [];
 
-  if(length > 13) {
+  if (nit.length > 12) {
     errors.push("TooLong");
   }
 
@@ -38,44 +49,25 @@ function calculateNITVerificationDigit(
     errors.push("InvalidFormat");
   }
 
-  let rangeMin = 0;
-  let rangeMax = 0;
-
-  // determine range and number of digits based on type of identification
-  if (nitDigits.length <= 8 && parseInt(nitDigits) <= 99999999) {
-    rangeMin = 1;
-    rangeMax = 99999999;
-  } else if (nitDigits.length <= 9 && parseInt(nitDigits) >= 700000001 && parseInt(nitDigits) <= 799999999) {
-    rangeMin = 700000001;
-    rangeMax = 799999999;
-  } else if (nitDigits.length <= 9 && parseInt(nitDigits) >= 600000001 && parseInt(nitDigits) <= 799999999) {
-    rangeMin = 600000001;
-    rangeMax = 799999999;
-  } else if (nitDigits.length <= 13 && parseInt(nitDigits) >= 1000000000) {
-    rangeMin = parseInt(nitDigits);
-    rangeMax = parseInt(nitDigits);
-  } else {
-    errors.push("InvalidFormat");
-  }
-
-  if (rangeMin > 0 && rangeMax > 0 && parseInt(nitDigits) < rangeMin || parseInt(nitDigits) > rangeMax) {
-    errors.push("InvalidFormat");
-  }
-
   if (errors.length > 0) {
     return makeLeft(Array.from(new Set(errors)));
   }
 
-  const nitArray = nitDigits.split("").map(Number);
-  const weights = [71, 67, 59, 53, 47, 43, 41, 37, 29, 23, 19, 17, 13, 7, 3];
+  const nitArray = addZeroesToLeft(nit.replace("-", "")).split("").map(Number);
+
+  const nitWeights = [71, 67, 59, 53, 47, 43, 41, 37, 29, 23, 19, 17, 13, 7, 3];
+
   let sum = 0;
 
   for (let i = 0; i < nitArray.length; i++) {
-    sum += nitArray[i] * weights[i];
+    sum += nitArray[i] * nitWeights[i];
   }
 
-  const mod = sum % 11;
-  const verificationDigit = mod === 0 || mod === 1 ? 0 : 11 - mod;
+  let verificationDigit = 11 - (sum % 11);
+
+  if (verificationDigit == 11) {
+    verificationDigit = 0;
+  }
 
   return makeRight({
     nit,
